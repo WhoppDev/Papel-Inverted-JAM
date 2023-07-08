e7875b7d2fd9407c5dc0e3217748c258fad015be
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-07-08 12:05:22 -0300

    Att

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,11 +4,9 @@ public class CharacterController : MonoBehaviour
 {
     public float velocidade = 5f;
 
-    [SerializeField] private ActionRecorder actionRecorder;
-
     private void Start()
     {
-        actionRecorder = FindObjectOfType<ActionRecorder>();
+
     }
 
     private void Update()
@@ -18,23 +16,15 @@ public class CharacterController : MonoBehaviour
 
     private void Move()
     {
-        // Obtém os inputs horizontais e verticais
         float movimentoHorizontal = Input.GetAxis("Horizontal");
         float movimentoVertical = Input.GetAxis("Vertical");
 
-        // Calcula a direção do movimento
         Vector2 direcao = new Vector2(movimentoHorizontal, movimentoVertical);
-
-        // Normaliza a direção para garantir um movimento suave em todas as direções
         direcao.Normalize();
 
-        // Calcula o vetor de deslocamento multiplicando a direção pelo tempo e pela velocidade
         Vector2 deslocamento = direcao * velocidade * Time.deltaTime;
 
-        // Move o objeto na direção calculada
         transform.Translate(deslocamento);
-
-        actionRecorder.RecordAction(transform.position, transform.rotation, Time.time, "Move");
     }
 
 }
