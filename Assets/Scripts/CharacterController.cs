fe5b8db492f349625a2432fb90bd9b61f4cdc4c4
Mateus Silva <132206573+mateussilvacustodio@users.noreply.github.com>
2023-07-08 17:06:17 -0300

    atualização

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,26 +3,31 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float velocidade = 5f;
-    public Transform ponto;
+    
+    public Transform[] ponto;
 
-    public GameObject location;
+    private int indice;
+
+    //public GameObject location;
 
     private void Start()
     {
-
+        indice = 0;
     }
 
     private void Update()
     {
         //Move();
-        Vector2 distancia = ponto.position - transform.position;
+        Vector2 distancia = ponto[indice].position - transform.position;
 
         distancia.Normalize();
         
         Vector2 targetPosition = (Vector2)transform.position + distancia * velocidade * Time.deltaTime;
 
         transform.position = targetPosition;
+
         
+
     }
 
     private void Move()
@@ -36,6 +41,28 @@ public class CharacterController : MonoBehaviour
         Vector2 deslocamento = direcao * velocidade * Time.deltaTime;
 
         transform.Translate(deslocamento);
+    }
+
+    void OnTriggerEnter2D (Collider2D collider) {
+
+        if(collider.gameObject.tag == "Point") {
+
+            if(indice < 7 ) {
+            
+            indice++;
+
+            } else {
+
+            print("Acabou");
+
+            }
+
+            
+
+
+
+        }
+
     }
 
 }
